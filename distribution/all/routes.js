fc51bae852093e0a5ec0ad958aed04cf2cac84aa
Forthoney <castlehoneyjung@gmail.com>
2024-04-07 22:46:29 -0400

    add more jsdoc

--- distribution/all/routes.js
+++ distribution/all/routes.js
@@ -1,15 +1,25 @@
+// @ts-check
 const comm = require("./comm");
+const types = require("../types");
 
-const routes = (config) => {
-  return {
-    put: (service, name, callback = () => {}) => {
-      comm(config).send(
-        [service, name],
-        { service: "routes", method: "put" },
-        callback,
-      );
-    },
-  };
-};
+/**
+ * @param {object} config
+ */
+function routes(config) {
+  /**
+   * @param {object} service
+   * @param {string} name
+   * @param {types.Callback} callback
+   */
+  function put(service, name, callback = () => {}) {
+    comm(config).send(
+      [service, name],
+      { service: "routes", method: "put" },
+      callback,
+    );
+  }
+
+  return { put };
+}
 
 module.exports = routes;
--- distribution/local/routes.js
+++ distribution/local/routes.js
@@ -1,28 +1,34 @@
+// @ts-check
+const types = require("../types");
+
+/** @type {Map.<string, object>} */
 const services = new Map();
 
-const routes = {
-  get: (service, callback = () => {}) => {
-    if (services.has(service)) {
-      return callback(null, services.get(service));
-    } else if (global.rpcLocal.has(service)) {
-      return callback(null, { call: global.rpcLocal.get(service) });
-    } else {
-      return callback(Error(`Route ${service} not found in services`), null);
-    }
-  },
-  /*
-   * Registers services under route
-   * If route already exists, it will check to see if the route is extensible -
-   * i.e. it is an object, not a singular value.
-   * If it is an object (e.g. Object, Array), it will merge the existing
-   * services under that route with the new services
-   * Otherwise, it will return an error.
-   */
-  put: (service, route, callback = () => {}) => {
-    services.set(route, service);
-    return callback(null, services.get(route));
-  },
-};
+/**
+ * @param {object} service
+ * @param {types.Callback} callback
+ */
+function get(service, callback = () => {}) {
+  if (services.has(service)) {
+    return callback(null, services.get(service));
+  } else if (global.rpcLocal.has(service)) {
+    return callback(null, { call: global.rpcLocal.get(service) });
+  } else {
+    return callback(Error(`Route ${service} not found in services`), null);
+  }
+}
+
+/**
+ * @param {object} service
+ * @param {string} route
+ * @param {types.Callback} callback
+ */
+function put(service, route, callback = () => {}) {
+  services.set(route, service);
+  return callback(null, services.get(route));
+}
+
+const routes = { get, put };
 
 services.set("status", require("./status"));
 services.set("groups", require("./groups"));
