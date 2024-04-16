867a15a1655c03bd8ada4de577133bce164dea97
Skyler Austen <me@skylerausten.com>
2024-04-15 22:23:00 -0400

    all keys in test check

--- test/reconf.test.js
+++ test/reconf.test.js
@@ -235,7 +235,9 @@ test('(2 pts) all.store.reconf(naiveHash)', (done) => {
           distribution.local.comm.send(msg, r6, (e, r6Values) => {
             console.log("r6", r6Values);
           });
-          console.log("FIRST CHECK VALUE", v);
+          distribution["group1"].store.get(null, (err, allKeys) => {
+            console.log("IN TEST", allKeys);
+          });
           expect(e).toBeFalsy();
           expect(v).toEqual(users[0]);
         } catch (error) {
