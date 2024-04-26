73859e1867eb9eb44f6ccaac9943f56505884c8b
Skyler Austen <me@skylerausten.com>
2024-04-26 10:01:14 -0400

    50

--- test/query.test.js
+++ test/query.test.js
@@ -217,7 +217,7 @@ test('Query for all sites with not included term', (done) => {
 });
 
 test('Repeated Query Total Performance Test', (done) => {
-  const numberOfQueries = 100;
+  const numberOfQueries = 50;
   const startTime = Date.now(); // Start timing before the first query
 
   const runQuery = (index) => {
