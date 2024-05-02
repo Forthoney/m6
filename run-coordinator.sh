5255cfa1074b9af290ef0b40bcbd6997805a3a96
Forthoney <castlehoneyjung@gmail.com>
2024-05-02 00:11:08 -0400

    add sleep

--- run-coordinator.sh
+++ run-coordinator.sh
@@ -2,6 +2,8 @@
 ip=$(hostname -i)
 ls ./data | grep output | while read -r line
 do
+	echo "Running batch $line"
+	sleep 2
 	./distribution.js --ip "$ip" --port 7070 &
 	./distribution.js --ip "$ip" --port 7080 &
 	./distribution.js --ip "$ip" --port 7090
