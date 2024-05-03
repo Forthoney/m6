bc6c5d256725d7d31575ddf32f8ce1161d8e25cc
Zhicheng Huang <skyhuang@umich.edu>
2024-05-02 20:43:39 -0400

    Added index script

--- run-batch-index.sh
+++ run-batch-index.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ip=$(hostname -i)
+./distribution.js --ip "$ip" --port 7070 --aws --index --prefixname map-seed-output_1 &
 ./distribution.js --ip "$ip" --port 7080 &
-./distribution.js --ip "$ip" --port 7090 &
-./distribution.js --ip "$ip" --port 7070 --aws --index --prefixname map-seed-output_0
+./distribution.js --ip "$ip" --port 7090
